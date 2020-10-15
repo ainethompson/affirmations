3147ddbbd38865773e51cbc68d09b1a2d5bc2c6a
Áine Thompson <ainegthompson@gmail.com>
2020-10-15 02:58:29 +0000

    /subscribe route

--- server.py
+++ server.py
@@ -13,15 +13,28 @@ def homepage():
     """ View Homepage. """
     return render_template('homepage.html')
 
+@app.route('/subscribe')
+def show_subscribe():
+    """ Show subscription page. """
+
+    return render_template('subscribe.html')
+
+@app.route('/subscribe', methods=['POST'])
+def process_subscribe():
+    """ Subscribe user to receive daily messages.
+
+    Save user's name and phone number to DB """
 
 
-@app.route('/messages')
-def all_messages():
-    """ View all messages. """
 
-    messages = crud.get_messages
 
-    return render_template('all_messages.html', messages=messages)
+# @app.route('/messages')
+# def all_messages():
+#     """ View all messages. """
+
+#     messages = crud.get_messages
+
+#     return render_template('all_messages.html', messages=messages)
 
 
 if __name__ == '__main__': 
